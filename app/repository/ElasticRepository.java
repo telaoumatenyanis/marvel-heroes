c50f9c1ef25a249c26014813051e3d55781854d2
yanis.telaoumaten <yanis.telaoumaten@sterblue.com>
2020-02-18 11:46:15 +0100

    feat: elasticsearch search is working

--- app/repository/ElasticRepository.java
+++ app/repository/ElasticRepository.java
@@ -1,5 +1,7 @@
 package repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import env.ElasticConfiguration;
 import env.MarvelHeroesConfiguration;
 import models.PaginatedResults;
@@ -9,6 +11,7 @@ import utils.SearchedHeroSamples;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -28,13 +31,34 @@ public class ElasticRepository {
 
 
     public CompletionStage<PaginatedResults<SearchedHero>> searchHeroes(String input, int size, int page) {
-        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        if (input.isEmpty()) {
+            input = "*";
+        }
+
+        String query = "{\n" +
+                "    \"size\": " + size + ",\n" +
+                "    \"from\": " + size * (page - 1) + ",\n" +
+                "    \"query\" : {\n" +
+                "                \"query_string\" : {\n" +
+                "                        \"fields\": [\"name^4\", \"secretIdentities^3\", \"aliases^3\", \"description^2\", \"partners^1\"],\n" +
+                "                        \"query\": \"" + input + "~\" }\n" +
+                "            }\n" +
+                "}";
+
+        CompletionStage<PaginatedResults<SearchedHero>> result = wsClient.url(elasticConfiguration.uri + "/heroes/_search").addHeader("Content-Type", "application/json").post(
+                query
+        ).thenApply(response -> {
+            final List<SearchedHero> heroes = new ArrayList<>();
+            final JsonNode hits = response.asJson().get("hits");
+            hits.get("hits").elements().forEachRemaining(e -> {
+                heroes.add(SearchedHero.fromJson(e.get("_source")));
+            });
+            int totalSize = hits.get("total").get("value").asInt();
+
+            return new PaginatedResults<>(totalSize, page, (int) Math.ceil(totalSize / size), heroes);
+        })  ;
+        System.out.println(result);
+        return result;
     }
 
     public CompletionStage<List<SearchedHero>> suggest(String input) {
